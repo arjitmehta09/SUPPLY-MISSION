c2ea58c296af4012d94799bb6266faf80ec63e71
arjitmehta09 <74130138+arjitmehta09@users.noreply.github.com>
2020-12-12 15:34:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
+
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
 var packageBody,ground
+var wall1,wall2,wall3
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -19,6 +21,7 @@ function setup() {
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG);
 	packageSprite.scale=0.2;
+	
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG);
@@ -27,10 +30,16 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255);
 
+	wall1=createSprite(400,650,200,20)
+	
+	wall2=createSprite(290,610,20,100)
+	
+	wall3=createSprite(510,610,20,100)
+
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
 
 	World.add(world, packageBody);
 	
@@ -48,6 +57,10 @@ function draw() {
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
+wall1.shapeColor="red";
+wall2.shapeColor="red";
+wall3.shapeColor="red";
+packageSprite.collide(wall1);
   drawSprites();
   keyPressed();
 }
@@ -59,6 +72,7 @@ function keyPressed() {
 
   }
 }
+
 
 
 
